31ef21481111331a06091e27ba8c39b88b11b3eb
박성빈 <psb106305@gmail.com>
2024-12-08 21:14:25 +0900

    feat: test

--- pir_sensor.py
+++ pir_sensor.py
@@ -9,18 +9,19 @@ def pir_monitoring(queue):
 
         while True:
             current_state = GPIO.input(PIR_PIN)  # 현재 상태 읽기
+            print(current_state)
 
             # 상태가 변경되었을 때만 처리
-            if current_state != previous_state:
-                if current_state == 1:  # 0 -> 1 (사람 감지)
-                    print("사람이 감지되었습니다!")
-                    queue.put(True)
-                else:  # 1 -> 0 (사람이 사라짐)
-                    print("사람이 사라졌습니다!")
-                    queue.put(False)
-
-                previous_state = current_state  # 상태 업데이트
-                time.sleep(2)  # 디바운싱
+            # if current_state != previous_state:
+            #     if current_state == 1:  # 0 -> 1 (사람 감지)
+            #         print("사람이 감지되었습니다!")
+            #         queue.put(True)
+            #     else:  # 1 -> 0 (사람이 사라짐)
+            #         print("사람이 사라졌습니다!")
+            #         queue.put(False)
+            #
+            #     previous_state = current_state  # 상태 업데이트
+            #     time.sleep(2)  # 디바운싱
 
             time.sleep(0.1)  # CPU 부하 감소
 
